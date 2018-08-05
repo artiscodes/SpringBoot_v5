9c9f9a1644599d8af89f14b7269e39e654d173eb
Art <artiscodes@gmail.com>
2018-08-05 16:58:10 +0200

    First commit

---
+++ src/main/java/artiscodes/Springboot_v4/Controller/ApplicationControler.java
@@ -0,0 +1,22 @@
+package artiscodes.Springboot_v4.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ApplicationControler {
+
+    @ResponseBody
+    @RequestMapping("/home")
+    public  String Hello(){
+
+        return "Hello from Spring";
+    }
+
+    @RequestMapping("/home")
+    public String Welcome(){
+        return "welcomepage";
+    }
+}
---
+++ src/main/java/artiscodes/Springboot_v4/MysqlWebSpringbootApplication.java
@@ -0,0 +1,12 @@
+package artiscodes.Springboot_v4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlWebSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlWebSpringbootApplication.class, args);
+	}
+}
